b5015550ffc8f7c23abb37e5a477dda129f97e6d
Yasmany Hernandez Echevarria <yasmany.hernandez@developers.net>
2023-11-15 21:51:57 -0600

    Day2

---
+++ src/app/shared/components/custom-input/custom-input.component.ts
@@ -0,0 +1,22 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-custom-input',
+  templateUrl: './custom-input.component.html',
+  styleUrls: ['./custom-input.component.scss'],
+})
+export class CustomInputComponent  implements OnInit {
+
+  @Input() control!: FormControl;
+  @Input() label!: string;
+  @Input() type!: string;
+  @Input() autocomplete!: string;
+  @Input() icon!: string;
+
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
